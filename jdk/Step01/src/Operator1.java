3b57a176d6a2dea6187daab81e0d114f42b718a4
Elzenaro <nam2626@gmail.com>
2021-11-24 14:14:04 +0900

    복습 예제

---
+++ jdk/Step01/src/Operator1.java
@@ -0,0 +1,12 @@
+
+public class Operator1 {
+	/*
+	 * 	정수형 변수 두개 선언하여 값은 임의로 초기화
+	 * 	모든 사칙연산의 결과를 출력 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ jdk/Step01/src/PrintTest.java
@@ -0,0 +1,14 @@
+
+public class PrintTest {
+	/*
+	 * 	Hello World 5번 출력하는 코드 작성
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+	}
+
+}
