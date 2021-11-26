340e06f81fce08d47d0b07058f674c6c073c0666
Elzenaro <nam2626@gmail.com>
2021-11-26 14:40:27 +0900

    메서드 예제

--- jdk/Step03/src/method/CalculatorMain.java
+++ jdk/Step03/src/method/CalculatorMain.java
@@ -5,6 +5,10 @@ public class CalculatorMain {
 	public static void main(String[] args) {
 		Calculator cal = new Calculator();
 		System.out.println(cal.sum(10, 20));
+		int n1 = cal.inputNumber();
+		int n2 = cal.inputNumber();
+		System.out.println(cal.sum(n1, n2));
+		
 	}
 
 }
