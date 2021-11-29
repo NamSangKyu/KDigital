40505689f5e86e0c39944b1d4309f76d5f8ac70a
Elzenaro <nam2626@gmail.com>
2021-11-29 17:04:53 +0900

    싱글톤, 팩토리 패턴 예제

--- jdk/Step04/src/singletone/CardMain.java
+++ jdk/Step04/src/singletone/CardMain.java
@@ -3,7 +3,15 @@ package singletone;
 public class CardMain {
 
 	public static void main(String[] args) {
-
+		Card card1 =  CardFactory.getInstance().createCard("홍길동");
+		Card card2 =  CardFactory.getInstance().createCard("김철수");
+		Card card3 =  CardFactory.getInstance().createCard("박영수");
+		Card card4 =  CardFactory.getInstance().createCard("이영희");
+		
+		card1.printCardInfo();
+		card2.printCardInfo();
+		card3.printCardInfo();
+		card4.printCardInfo();
 	}
 
 }
