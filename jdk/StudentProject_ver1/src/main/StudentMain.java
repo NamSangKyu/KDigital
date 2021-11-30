0fb46d79199031b823aa5da5d6964bc2349bfcee
Elzenaro <nam2626@gmail.com>
2021-11-30 09:24:08 +0900

    메인 클래스

---
+++ jdk/StudentProject_ver1/src/main/StudentMain.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.util.Scanner;
+
+import service.StudentService;
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		StudentService service = StudentService.getInstance();
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			/*
+			 *	1. 학생정보 추가 
+			 *	2. 학생정보 검색
+			 *	3. 학생정보 수정
+			 *	4. 학생정보 삭제
+			 *	5. 전체 학생정보 조회
+			 *	0. 프로그램 종료
+			 *  원하시는 메뉴 번호를 입력하세요 :  
+			 */
+			
+		}
+		
+	}
+
+}
+
+
+
+
