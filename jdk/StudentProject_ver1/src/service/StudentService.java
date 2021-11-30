5af830972dfa645eef61ab405c7db26a110d00bc
Elzenaro <nam2626@gmail.com>
2021-11-30 09:18:00 +0900

    서비스 클래스 작성

--- jdk/StudentProject_ver1/src/service/StudentService.java
+++ jdk/StudentProject_ver1/src/service/StudentService.java
@@ -1,5 +1,22 @@
 package service;
+
+import vo.StudentVO;
+
 //싱글톤 패턴 적용
 public class StudentService {
+	private static StudentService instance = new StudentService();
+	private StudentVO[] arr;
+	private int idx;
+	
+	private StudentService() {
+		arr = new StudentVO[5];
+	}
 
+	public static StudentService getInstance() {
+		if(instance == null)
+			instance = new StudentService();
+		return instance;
+	}
+
+	
 }
