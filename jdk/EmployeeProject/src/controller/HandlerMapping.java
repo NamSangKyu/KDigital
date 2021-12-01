44bab5c9b3061314cc756011ec69a9406e623409
Elzenaro <nam2626@gmail.com>
2021-12-01 17:34:57 +0900

    사원관리 예제

--- jdk/EmployeeProject/src/controller/HandlerMapping.java
+++ jdk/EmployeeProject/src/controller/HandlerMapping.java
@@ -13,7 +13,11 @@ public class HandlerMapping {
 	
 	public Controller createController(int no) {
 		Controller controller = null;
-		
+		switch(no) {
+		case 1:
+			controller = new RegisterController();
+			break;
+		}
 		return controller;
 	}
 	
---
+++ jdk/EmployeeProject/src/controller/RegisterController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import java.util.Scanner;
+
+public class RegisterController extends Controller {
+	@Override
+	public void execute(Scanner sc) {
+		//사원정보 입력
+		//1. 일반 직원
+		//2. 영업직
+		//3. 파트타임
+		//4. 파견직
+		// 입력 받아서 해당 Employee 생성에 필요한 데이터를 입력
+	}
+}
