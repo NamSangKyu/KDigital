954c11f62480bd98da924307d77bb637f0f8dba8
Elzenaro <nam2626@gmail.com>
2021-12-02 10:07:11 +0900

    사원 정보 등록 예제

--- jdk/EmployeeProject/src/service/EmployeeService.java
+++ jdk/EmployeeProject/src/service/EmployeeService.java
@@ -17,7 +17,23 @@ public class EmployeeService {
 	}
 
 	public void insertEmployee(EmployeeVO employee) {
-		
+		//배열에 남은 공간이 있는지 체크
+		if(index == list.length) {
+			System.out.println("더이상 저장할 공간이 없습니다.");
+			return ;
+		}
+		list[index] = employee;
+		index++;
+		System.out.println("사원 정보 등록이 완료 되었습니다.");
 	}
 	
 }
+
+
+
+
+
+
+
+
+
