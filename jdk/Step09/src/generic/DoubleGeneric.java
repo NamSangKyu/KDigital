17dc409de0db5fba26f1b92f1b27e11409a2d54b
Elzenaro <nam2626@gmail.com>
2021-12-06 10:05:45 +0900

    제네릭 예제

---
+++ jdk/Step09/src/generic/DoubleGeneric.java
@@ -0,0 +1,22 @@
+
+package generic;
+
+public class DoubleGeneric <T1,T2>{
+	T1 obj1;
+	T2 obj2;
+	public T1 getObj1() {
+		return obj1;
+	}
+	public void setObj1(T1 obj1) {
+		this.obj1 = obj1;
+	}
+	public T2 getObj2() {
+		return obj2;
+	}
+	public void setObj2(T2 obj2) {
+		this.obj2 = obj2;
+	}
+	
+	
+
+}
---
+++ jdk/Step09/src/generic/DoubleGenericMain.java
@@ -0,0 +1,7 @@
+package generic;
+
+public class DoubleGenericMain {
+	public static void main(String[] args) {
+		DoubleGeneric<Integer, String> dg = new DoubleGeneric<Integer, String>();
+	}
+}
