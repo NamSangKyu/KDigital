346a031c9157ecd1965f49ea49c62840cfe058f5
Elzenaro <nam2626@gmail.com>
2021-12-06 09:42:38 +0900

    제네릭 예제

--- jdk/Step09/src/generic/BoxGenericMain.java
+++ jdk/Step09/src/generic/BoxGenericMain.java
@@ -3,7 +3,15 @@ package generic;
 public class BoxGenericMain {
 
 	public static void main(String[] args) {
+		BoxGeneric<Animal> box1 = new BoxGeneric<Animal>();
+		box1.setObj(new Animal());
+		Animal animal = box1.getObj();
+		animal.printAnimal();
 		
+		BoxGeneric<Apple> box2 = new BoxGeneric<Apple>();
+		box2.setObj(new Apple());
+		Apple apple = box2.getObj();
+		apple.printApple();
 	}
 
 }
