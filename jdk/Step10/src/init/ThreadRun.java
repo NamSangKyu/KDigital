4372cc4ff5cbc4edb9b871bf4c8806d46561d7fe
Elzenaro <nam2626@gmail.com>
2021-12-07 17:21:45 +0900

    스레드 클래스 예제

---
+++ jdk/Step10/src/init/ThreadRun.java
@@ -0,0 +1,19 @@
+package init;
+
+public class ThreadRun extends Thread{
+
+	//생성자 --> 스레드 이름을 받음
+	public ThreadRun(String name) {
+		super(name);
+		System.out.println(name + " - 스레드 생성");
+	}
+	
+	//자동으로 수행할 메서드
+	@Override
+	public void run() {
+		for(int i=0;i<100;i++)
+			System.out.println(i + " " + getName());
+		System.out.println(getName() + " - 스레드 종료");
+	}
+
+}
---
+++ jdk/Step10/src/init/ThreadTest.java
@@ -0,0 +1,9 @@
+package init;
+
+public class ThreadTest {
+
+	public static void main(String[] args) {
+
+	}
+
+}
