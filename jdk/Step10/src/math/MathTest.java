fae83cc4457eb6ad1ffd8583661b61bef7891d88
Elzenaro <nam2626@gmail.com>
2021-12-07 10:01:15 +0900

    Math 클래스

---
+++ jdk/Step10/src/math/MathTest.java
@@ -0,0 +1,28 @@
+package math;
+
+public class MathTest {
+
+	public static void main(String[] args) {
+		//절대값
+		System.out.println(Math.abs(-10));
+		//로그
+		System.out.println(Math.log(3.56));
+		//삼각함수
+		System.out.println(Math.sin(45));
+		System.out.println(Math.cos(45));
+		System.out.println(Math.tan(45));
+		//반올림 - 소수점 반올림
+		System.out.println(Math.round(3.56456));
+		//거듭제곱
+		System.out.println(Math.pow(2, 10));
+		//0.0이상 1.0 미만의 난수
+		System.out.println(Math.random());
+	}
+
+}
+
+
+
+
+
+
