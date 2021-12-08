8d6f711e028100567cd6d78c9a3562e3ba713ddb
Elzenaro <nam2626@gmail.com>
2021-12-08 15:20:55 +0900

    스트림 예제

--- jdk/Step11/src/io/IOTest.java
+++ jdk/Step11/src/io/IOTest.java
@@ -19,10 +19,12 @@ public class IOTest {
 				buffer[idx] = (char) isr.read();// 한글자씩 입력 받음
 				idx++;
 			} while (buffer[idx - 1] != '\n');
-			//System.out.println(Arrays.toString(buffer));
-			for(int i=0;i<idx;i++) {
-				System.out.print((int)buffer[i] + " ");
+			// System.out.println(Arrays.toString(buffer));
+
+			for (int i = 0; i < idx; i++) {
+				System.out.print((int) buffer[i] + " ");
 			}
+
 			System.out.println();
 			message = String.copyValueOf(buffer, 0, idx - 2);
 		} catch (IOException e) {
