d9cf43d55f0055ec6c9d89ce8b4f9f045bf4a2ca
Elzenaro <nam2626@gmail.com>
2021-12-13 15:51:26 +0900

    연산자, 조건문

---
+++ C#/Step02/Step02/IfIf.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step02
+{
+    internal class IfIf
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("숫자를 입력하세요. : ");
+
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            if (number > 0)
+            {
+                if (number % 2 == 0)
+                    Console.WriteLine("0보다 큰 짝수.");
+                else
+                    Console.WriteLine("0보다 큰 홀수.");
+            }
+            else
+            {
+                Console.WriteLine("0보다 작거나 같은 수.");
+            }
+        }
+    }
+}
+
