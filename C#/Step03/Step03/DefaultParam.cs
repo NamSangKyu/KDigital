26a5d9b2d4b52b73f14bd5099add608d40bc05cb
Elzenaro <nam2626@gmail.com>
2021-12-14 12:46:21 +0900

    디폴트 값

---
+++ C#/Step03/Step03/DefaultParam.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step03
+{
+    internal class DefaultParam
+    {
+        public static int sum(int n)
+        {
+            return n + n;
+        }
+        public static int sum(int n1=10,int n2=20)
+        {
+            return n1 + n2;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sum(10, 20));
+            Console.WriteLine(sum());
+            Console.WriteLine(sum(10));
+        }
+    }
+}
