398ab5499424fd8e532057c5692ab4fe9798d277
Elzenaro <nam2626@gmail.com>
2021-12-15 10:28:25 +0900

    배열, 인덱서

---
+++ C#/Step06/Step06/Array.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step06
+{
+    internal class Array
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[5];
+            arr1[0] = 1;
+            arr1[1] = 2;
+            arr1[2] = 3;
+            arr1[3] = 4;
+            arr1[4] = 5;
+
+            for(int i=0; i<arr1.Length; i++)
+                Console.WriteLine(arr1[i]);
+
+            Console.WriteLine("-------");
+            
+            int[] arr2 = new int[5] {1,2,3,4,5};
+            
+            foreach(int n in arr2)
+                Console.WriteLine(n);
+            
+            Console.WriteLine("-------");
+
+            int[] arr3 = new int[] {1,2,3,4,5};
+
+            foreach(int n in arr3)
+                Console.WriteLine(n);
+        }
+    }
+}
---
+++ C#/Step06/Step06/Indexer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step06
+{
+    public class CustomList
+    {
+        private int[] arr;
+
+        public CustomList()
+        {
+            arr = new int[10];
+        }
+
+        public int this[int index]
+        {
+            get { return arr[index]; }
+            set
+            {
+                if(index >= arr.Length)
+                {
+                    Array.Resize(ref arr, index+1);
+                    Console.WriteLine($"배열 크기 증가 : {arr.Length}");
+                }
+                arr[index] = value;
+            }
+        }
+        //배열 크기 리턴하는 프로퍼티 작성
+        public int Length { get { return arr.Length; } }
+    }
+    internal class Indexer
+    {
+        static void Main(string[] args)
+        {
+            CustomList list = new CustomList();
+
+            for(int i=0; i < 15; i++){
+                list[i] = i + 1;
+            }
+
+            for(int i=0; i < list.Length; i++)
+            {
+                Console.WriteLine(list[i]); 
+            }
+        }
+    }
+}
