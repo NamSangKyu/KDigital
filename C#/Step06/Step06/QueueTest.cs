5ba0b532f67409c5950469a6cdeda77f12905646
Elzenaro <nam2626@gmail.com>
2021-12-15 12:03:55 +0900

    큐

---
+++ C#/Step06/Step06/QueueTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step06
+{
+    internal class QueueTest
+    {
+        static void Main(string[] args){
+            Queue que = new Queue();
+            que.Enqueue(1);
+            que.Enqueue(2);
+            que.Enqueue(3);
+            que.Enqueue(4);
+            que.Enqueue(5);
+
+            while (que.Count > 0)
+                Console.WriteLine(que.Dequeue());
+        }
+    }
+}
