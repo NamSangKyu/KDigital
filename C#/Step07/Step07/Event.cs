de57ff5c67160b818547bbafc424471176ee549c
Elzenaro <nam2626@gmail.com>
2021-12-15 14:53:41 +0900

    대리자

---
+++ C#/Step07/Step07/Event.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step07
+{
+    delegate void EventHandler(string message);
+
+    class Mynotifier
+    {
+        public event EventHandler handler;
+        public void run(int number)
+        {
+            if (number % 2 == 0)
+                handler($"{number}는 짝수 입니다.");
+        }
+    }
+    internal class Event
+    {
+        public static void Handler(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void Main(string[] args)
+        {
+            Mynotifier notifier = new Mynotifier();
+            notifier.handler += Handler;
+            notifier.run(10);
+        }
+    }
+}
