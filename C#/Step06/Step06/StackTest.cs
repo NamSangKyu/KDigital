a51cd3db808fdafd199e77547bcc7b569c351f6d
Elzenaro <nam2626@gmail.com>
2021-12-15 11:26:48 +0900

    스택

---
+++ C#/Step06/Step06/StackTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step06
+{
+    internal class StackTest
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            while (stack.Count > 0)
+                Console.WriteLine(stack.Pop());
+
+            stack.Push("https://www.naver.com");
+            stack.Push("https://www.daum.net");
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
