8f034ed04fadadeb6f7c92b289b46e162d749450
Elzenaro <nam2626@gmail.com>
2021-12-16 09:47:04 +0900

    학생정보관리 프로젝트

---
+++ C#/Step08/Step08/LINQ_1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step08
+{
+    internal class LINQ_1
+    {
+        /*
+         * from 데이터 집합
+         * where 조건
+         * orderby 정렬
+         * select 추출할 데이터
+         */
+        static void Main(string[] args)
+        {
+            int[] numbers = { 9, 2, 6, 4, 5, 3, 7, 8, 1, 10 };
+
+            var result = from n in numbers
+                         where n % 2 == 0
+                         orderby n descending   
+                         select n;
+
+            foreach (int n in result)
+                Console.Write($"{n} ");
+
+
+        }
+    }
+}
---
+++ C#/Step08/Step08/LINQ_2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step08
+{
+    class Profile
+    {
+        private string name;
+        private int height;
+        public string Name { get => name; set => name = value; }
+        public int Height { get => height; set => height = value; }
+    }
+    internal class LINQ_2
+    {
+        static void Main(string[] args)
+        {
+            Profile[] arrProfile =
+            {
+                new Profile(){Name="정우성", Height=186},
+                new Profile(){Name="김태희", Height=158},
+                new Profile(){Name="고현정", Height=172},
+                new Profile(){Name="이문세", Height=178},
+                new Profile(){Name="하하", Height=167}
+            };
+
+            var profiles = from profile in arrProfile
+                           where profile.Height < 175
+                           orderby profile.Height
+                           select new Profile { 
+                               Name=profile.Name, 
+                               Height=profile.Height
+                           };
+            foreach(Profile p in profiles)
+                Console.WriteLine($"{p.Name} {p.Height}");
+        }
+    }
+}
