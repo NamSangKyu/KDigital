c792df589031621338746967b7acbdfce9b72de9
kimyh1216 <kimyh1216@gmail.com>
2022-01-20 10:40:55 +0900

    쿠키 설정

---
+++ jsp/Step02/src/main/java/cookie/CookieServlet.java
@@ -0,0 +1,46 @@
+package cookie;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CookieServlet
+ */
+@WebServlet("/CookieServlet")
+public class CookieServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CookieServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Cookie cookie = new Cookie("cookie", request.getParameter("data"));
+		cookie.setMaxAge(60);//쿠키 만료시간 설정, 초단위
+		cookie.setPath("/");
+		response.addCookie(cookie);
+		
+		response.sendRedirect("cookie_result.jsp");   
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
