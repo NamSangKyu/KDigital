d7b78141217fdb67e6ae5d3b627cadf281e0302c
kimyh1216 <kimyh1216@gmail.com>
2022-01-20 11:45:45 +0900

    DTO 저장

---
+++ jsp/Step02/src/main/java/person/PersonServlet.java
@@ -0,0 +1,50 @@
+package person;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PersonServlet
+ */
+@WebServlet("/PersonServlet")
+public class PersonServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PersonServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name= request.getParameter("name");
+		int age = Integer.parseInt(request.getParameter("age"));
+		
+		PersonDTO p = new PersonDTO(name, age);
+		
+		request.setAttribute("person", p);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("person_result.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ jsp/Step02/src/main/webapp/person.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="PersonServlet">
+		<input type="text" name="name" placeholder="이름 입력하세요"><br>
+		<input type="text" name="age" placeholder="나이를 입력하세요"><br>
+		<button>전송</button>
+	</form>
+</body>
+</html>
