671671de9c5ccb5bd30bf0fd70e3a3e39e65385a
kimyh1216 <kimyh1216@gmail.com>
2022-01-21 11:47:16 +0900

    로그인

--- jsp/StudentProject/src/main/java/dao/StudentDAO.java
+++ jsp/StudentProject/src/main/java/dao/StudentDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import config.DBManager;
 import dto.StudentDTO;
@@ -50,5 +51,45 @@ public class StudentDAO {
 		
 		return dto;
 	}
-
+	public ArrayList<StudentDTO> selectAllStudent(){
+		ArrayList<StudentDTO> list = new ArrayList<StudentDTO>();
+		String sql = "select * from student";
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;		
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				list.add(new StudentDTO(rs.getString(1), rs.getString(2), 
+						rs.getInt(3), rs.getDouble(4)));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null)rs.close();
+				if(pstmt != null)pstmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
