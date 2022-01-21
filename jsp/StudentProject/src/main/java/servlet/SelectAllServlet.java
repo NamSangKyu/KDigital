825dde0a7f937cdf706a119f348ec9c578ee07e6
kimyh1216 <kimyh1216@gmail.com>
2022-01-21 15:49:11 +0900

    학생정보 추가

---
+++ jsp/StudentProject/src/main/java/servlet/SelectAllServlet.java
@@ -0,0 +1,44 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SelectAllServlet
+ */
+@WebServlet("/SelectAllServlet")
+public class SelectAllServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SelectAllServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//전체 학생 데이터 셋팅
+        
+		//전체 학과 데이터 셋팅
+		
+		//student_list.jsp로 페이지 이동
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
