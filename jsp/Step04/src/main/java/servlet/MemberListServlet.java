ba8bdef7f6c5476e72fef790928faea993b457bc
NamSangKyu <nam2626@gmail.com>
2022-01-24 14:25:16 +0900

    ajax

---
+++ jsp/Step04/src/main/java/servlet/MemberListServlet.java
@@ -0,0 +1,51 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+
+import dao.MemberDAO;
+import dto.MemberDTO;
+
+/**
+ * Servlet implementation class MemberListServlet
+ */
+@WebServlet("/MemberListServlet")
+public class MemberListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MemberListServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		ArrayList<MemberDTO> list = MemberDAO.getInstance().selectMember();
+		JSONArray arr = new JSONArray(list);
+		System.out.println(arr.toString());
+		response.getWriter().write(arr.toString());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
