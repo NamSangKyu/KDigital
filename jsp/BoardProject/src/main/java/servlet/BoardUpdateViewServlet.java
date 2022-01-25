c92945102d98c39d1c00000bfe2beb66c282ab11
NamSangKyu <nam2626@gmail.com>
2022-01-25 16:28:57 +0900

    게시판

---
+++ jsp/BoardProject/src/main/java/servlet/BoardUpdateViewServlet.java
@@ -0,0 +1,46 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dto.BoardDTO;
+import service.BoardService;
+
+/**
+ * Servlet implementation class BoardUpdateViewServlet
+ */
+@WebServlet("/boardUpdateView.do")
+public class BoardUpdateViewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BoardUpdateViewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int bno = Integer.parseInt(request.getParameter("bno"));
+		BoardDTO dto = BoardService.getInstance().selectBoard(bno);
+		request.setAttribute("dto", dto);		
+		request.getRequestDispatcher("board/board_update_view.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
