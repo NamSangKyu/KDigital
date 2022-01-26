7c288a60d8dad49022899516eb171f12fe44ccd7
kimyh1216 <kimyh1216@gmail.com>
2022-01-26 14:37:41 +0900

    spring

--- spring/SpringMVC/src/main/java/org/mvc/MainController.java
+++ spring/SpringMVC/src/main/java/org/mvc/MainController.java
@@ -1,8 +1,14 @@
 package org.mvc;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.mvc.dto.StudentDTO;
 import org.mvc.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,14 +29,36 @@ public class MainController {
 	}
 	
 	@RequestMapping("/login.do")
-	public String login(HttpServletRequest request, HttpSession session) {
+	public String login(HttpServletRequest request, HttpSession session,
+			HttpServletResponse response) throws IOException {
 		String sno = request.getParameter("sno");
 		String name = request.getParameter("name");
 		
 		System.out.println(sno + " " + name);
+		StudentDTO dto = studentService.login(sno, name);
+		if(dto == null) {
+			response.setContentType("text/html;charset=utf-8");
+			response.getWriter().write("<script>alert('학번과 이름을 확인하세요');history.back();</script>");
+			return null;//페이지 이동 X
+		}
+		session.setAttribute("dto", dto);
+
+		return "redirect:list.do";
+	}
+	
+	@RequestMapping("/list.do")
+	public String list(HttpServletRequest request) {
+		//전체 데이터가지고 와서 셋팅
+		ArrayList<StudentDTO> list = studentService.selectAllStudnet();
+		ArrayList<HashMap<String, Object>> major = studentService.selectAllMajor();
+		
+		request.setAttribute("list", list);
+		request.setAttribute("major", major);
+		
 		
 		return "student_list";
 	}
+	
 }
 
 
