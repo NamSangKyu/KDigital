fee9973bfd18c2b06b2c679da034c5ecaf1e7980
kimyh1216 <kimyh1216@gmail.com>
2022-01-27 15:50:46 +0900

    Spring Board

---
+++ spring/SpringBoard/src/main/java/com/board/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.board;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBoardApplication.class);
+	}
+
+}
