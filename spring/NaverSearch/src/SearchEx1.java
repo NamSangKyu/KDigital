68578852960794df08fa177ec1f71a803f435634
kimyh1216 <kimyh1216@gmail.com>
2022-02-03 11:02:04 +0900

    네이버 검색

--- spring/NaverSearch/src/SearchEx1.java
+++ spring/NaverSearch/src/SearchEx1.java
@@ -49,8 +49,93 @@ public class SearchEx1 {
 
 		return msg;
 	}
+	//도서 검색
+	public static String searchBook(String text) {
+		String clientId = "KHkektMREM4uRRubtXSl";// 애플리케이션 클라이언트 아이디값";
+		String clientSecret = "trD4iQ7zjS";// 애플리케이션 클라이언트 시크릿값";
+		String apiURL = "https://openapi.naver.com/v1/search/book.json?query=";
+		String msg = null;
+		HttpURLConnection con = null;
+		DataOutputStream dos = null;
+		BufferedReader br = null;
+
+		try {
+			text = URLEncoder.encode(text, "UTF-8");
+			// url 연결
+			URL url = new URL(apiURL+text);
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			con.setRequestProperty("X-Naver-Client-Id", clientId);
+			con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+			// 데이터 전송
+			con.setDoOutput(true);
+			int responseCode = con.getResponseCode();
+			if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 응답
+				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			} else { // 에러 응답
+				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+			}
+			msg = new String();
+			while (true) {
+				String str = br.readLine();
+				if (str == null)
+					break;
+				msg += str;
+			}
+			
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return msg;
+	}
+	//뉴스
+	public static String searchNews(String text) {
+		String clientId = "KHkektMREM4uRRubtXSl";// 애플리케이션 클라이언트 아이디값";
+		String clientSecret = "trD4iQ7zjS";// 애플리케이션 클라이언트 시크릿값";
+		String apiURL = "https://openapi.naver.com/v1/search/news.json?query=";
+		String msg = null;
+		HttpURLConnection con = null;
+		DataOutputStream dos = null;
+		BufferedReader br = null;
+
+		try {
+			text = URLEncoder.encode(text, "UTF-8");
+			// url 연결
+			URL url = new URL(apiURL+text);
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			con.setRequestProperty("X-Naver-Client-Id", clientId);
+			con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+			// 데이터 전송
+			con.setDoOutput(true);
+			int responseCode = con.getResponseCode();
+			if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 응답
+				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			} else { // 에러 응답
+				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+			}
+			msg = new String();
+			while (true) {
+				String str = br.readLine();
+				if (str == null)
+					break;
+				msg += str;
+			}
+			
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return msg;
+	}
 	public static void main(String[] args) {
 		String msg = searchBlog("오미크론");
+		msg = searchNews("오미크론");
 		System.out.println(msg);
 	}
 
