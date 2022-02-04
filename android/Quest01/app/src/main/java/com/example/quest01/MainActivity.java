59b9030240204fc442a12948355b6d6a7bf736f9
kimyh1216 <kimyh1216@gmail.com>
2022-02-04 15:39:50 +0900

    첫번째 문재

--- android/Quest01/app/src/main/java/com/example/quest01/MainActivity.java
+++ android/Quest01/app/src/main/java/com/example/quest01/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.quest01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imgView1;
+    ImageView imgView2;
+    ScrollView scroll1;
+    BitmapDrawable bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //가로 스크롤바 설정
+        scroll1 = findViewById(R.id.scroll1);
+        scroll1.setHorizontalScrollBarEnabled(true);
+
+        //이미지뷰 연결
+        imgView1 = findViewById(R.id.imgView1);
+        imgView2 = findViewById(R.id.imgView2);
+        //이미지 로드
+        Resources res = getResources();
+        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.pier);
+
+        //이미지 설정
+        imgView1.setImageDrawable(bitmap);
+        imgView2.setImageDrawable(null);
+
+
+
     }
 }
