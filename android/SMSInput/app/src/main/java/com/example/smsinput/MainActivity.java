75abbb4d4d0f40857576df5879c3bee132dac68f
kimyh1216 <kimyh1216@gmail.com>
2022-02-07 17:15:46 +0900

    TEXT 이벤트 처리

--- android/SMSInput/app/src/main/java/com/example/smsinput/MainActivity.java
+++ android/SMSInput/app/src/main/java/com/example/smsinput/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.smsinput;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,5 +29,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button btnSend = findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, edtContent.getText().toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        edtContent.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                int count = charSequence.length();
+                Log.d("SMSInput", "onTextChanged: 글자 개수 : "+count);
+                txtLength.setText(count + " / 80 글자");
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
