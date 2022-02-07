42a162101b82de4f2ff3ea12d23e1e4b6b147374
kimyh1216 <kimyh1216@gmail.com>
2022-02-07 17:05:05 +0900

    TEXT 이벤트 처리

--- android/SMSInput/app/src/main/java/com/example/smsinput/MainActivity.java
+++ android/SMSInput/app/src/main/java/com/example/smsinput/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.smsinput;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText edtContent = findViewById(R.id.edtContent);
+        TextView txtLength = findViewById(R.id.txtLength);
+
+        Button btnExit = findViewById(R.id.btnExit);
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finishAndRemoveTask();
+            }
+        });
+
     }
 }
