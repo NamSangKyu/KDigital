99977a1ee2f6c4015e701587ef375e0d1e9907d5
kimyh1216 <kimyh1216@gmail.com>
2022-02-08 15:45:45 +0900

    ProgressBar

---
+++ android/ProgressBar/app/src/main/java/com/example/progressbar/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.progressbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ProgressBar bar = findViewById(R.id.progress);
+        bar.setProgress(20);
+        Button btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bar.incrementProgressBy(10);
+            }
+        });
+        Button btnReset = findViewById(R.id.btnReset);
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bar.setProgress(0);
+            }
+        });
+        Button btnAlert = findViewById(R.id.btnAlert);
+        btnAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+                dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                dialog.setMessage("데이터 로드 중입니다.");
+                dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                    @Override
+                    public void onShow(DialogInterface dialogInterface) {
+                        Runnable r = new Runnable() {
+                            @Override
+                            public void run() {
+                                while(dialog.getProgress() < 100) {
+                                    try {
+                                        Thread.sleep(1000);
+                                        dialog.incrementProgressBy(5);
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                dialog.dismiss();
+                            }
+                        };
+                        Thread t = new Thread(r);
+                        t.start();
+
+                    }
+                });
+                dialog.setCanceledOnTouchOutside(false);
+                dialog.setButton(ProgressDialog.BUTTON_POSITIVE, "예", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //예 버튼 눌렀을때 처리
+                        dialog.dismiss();//창 종료
+                    }
+                });
+                dialog.setButton(ProgressDialog.BUTTON_NEUTRAL, "취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                dialog.show();
+
+            }
+        });
+
+    }
+}
+
+
+
+
+
+
+
+
+
