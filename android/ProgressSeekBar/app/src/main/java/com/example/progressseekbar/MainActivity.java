960cc2c334f7f0b4db0e319878bf6b4cef1bd257
kimyh1216 <kimyh1216@gmail.com>
2022-02-08 16:38:33 +0900

    ProgressBar

---
+++ android/ProgressSeekBar/app/src/main/java/com/example/progressseekbar/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.progressseekbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        EditText editText = findViewById(R.id.edtValue);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                progressBar.setProgress(i);
+                editText.setText(String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
+
+
+
+
+
+
+
