54b0df4e30e6f174b48cfd6bef3f68b7e6957b2e
NamSangKyu <nam2626@gmail.com>
2022-02-16 18:42:39 +0900

    문제 풀이

--- android/RegisterApp/app/src/main/java/com/example/registerapp/RegisterFinActivity.java
+++ android/RegisterApp/app/src/main/java/com/example/registerapp/RegisterFinActivity.java
@@ -2,7 +2,9 @@ package com.example.registerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RegisterFinActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class RegisterFinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_fin);
+        TextView txtResult = findViewById(R.id.txt_result);
+        Intent intent = getIntent();
+        String msg = intent.getStringExtra("id")+"님 회원가입이 완료되었습니다.";
+        txtResult.setText(msg);
     }
 }
